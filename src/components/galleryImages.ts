d9841cae19f9cd6c8a378774a606f8229bc2624b
Shyju Viswambaran <shyjuezy@gmail.com>
2024-11-02 01:58:07 -0500

    Fixed the gallery layout

---
+++ src/components/galleryImages.ts
@@ -0,0 +1,53 @@
+export interface GalleryImage {
+  alt: string;
+  src: string;
+  rowSpan: string;
+}
+
+export const images: GalleryImage[] = [
+  {
+    alt: "Picture",
+    src: "/images/gallery-pictures-square-1.jpg",
+    rowSpan: "row-span-3",
+  },
+  {
+    alt: "Some other pic",
+    src: "/images/gallery-pictures-vertical-small.jpg",
+    rowSpan: "row-span-1",
+  },
+  {
+    alt: "Another pic",
+    src: "/images/gallery-pictures-vertical-3.jpg",
+    rowSpan: "row-span-4",
+  },
+  {
+    alt: "Another pic",
+    src: "/images/gallery-pictures-vertical-2.jpg",
+    rowSpan: "row-span-5",
+  },
+  {
+    alt: "Another pic",
+    src: "/images/gallery-pictures-vertical-4.jpg",
+    rowSpan: "row-span-5",
+  },
+  {
+    alt: "Another pic",
+    src: "/images/gallery-pictures-vertical-1.jpg",
+    rowSpan: "row-span-4",
+  },
+  {
+    alt: "Another another pic",
+    src: "/images/gallery-pictures-square-2.jpg",
+    rowSpan: "row-span-3",
+  },
+  {
+    alt: "Another another pic",
+    src: "/images/gallery-pictures-square-3.jpg",
+    rowSpan: "row-span-3",
+  },
+  {
+    alt: "Another another pic",
+    src: "/images/gallery-pictures-square-4.jpg",
+    rowSpan: "row-span-2",
+  },
+];
