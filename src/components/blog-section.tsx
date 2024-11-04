605b84b141f35814629bf98593090d56aee25718
Shyju Viswambaran <shyjuezy@gmail.com>
2024-11-03 19:39:53 -0600

    fixed the type issue

--- src/components/blog-section.tsx
+++ src/components/blog-section.tsx
@@ -1,6 +1,6 @@
 import Link from "next/link";
 import Image from "next/image";
-import { blogSections, BlogSection } from "@/data/blogSection";
+import { blogSections, type BlogSection } from "@/data/blogSection";
 
 import {
   Carousel,
