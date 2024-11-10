d6d5a1012c9da2e72ff0f338258a2182ca85950c
Shyju Viswambaran <shyjuezy@gmail.com>
2024-11-09 21:50:25 -0600

    Added about-us page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,4 @@
-import { Header } from "@/components/header";
 import { Gallery } from "@/components/gallery";
-import { Footer } from "@/components/footer";
 import { FeaturedProducts } from "@/components/featured-products";
 import { HeroSection } from "@/components/hero-section";
 import TestimonialSection from '@/components/testimonial-section'
